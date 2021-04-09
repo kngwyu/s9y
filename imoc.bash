1ee225014843f7ac0369dbcaab1d2291667c68e2
kngwyu <yuji.kngw.80s.revive@gmail.com>
2021-04-09 16:04:33 +0900

    Use array for options

--- imoc.bash
+++ imoc.bash
@@ -14,8 +14,8 @@ git clone git@github.com:kngwyu/private-experiments.git && \
 echo "export S9Y_CONTAINER=\"$HOME/singularity-containers/py38-torch181-cuda111-mujoco200.sif\"" >> .s9y-env
 echo "export S9Y_VENV=\"$HOME/imoc/.imoc-venv=\"" >> .s9y-env
 
-s9y venv --create
-s9y venv pip install -e file://mujoco-maze
-s9y venv pip install -e file://Rainy
-s9y venv pip install -e file://intrinsic-rewards
-s9y venv pip install -e file://rlpy3
+s9y venv --create && \
+    s9y venv pip install -e ./mujoco-maze && \
+    s9y venv pip install -e ./Rainy && \
+    s9y venv pip install -e ./intrinsic-rewards && \
+    s9y venv pip install -e ./rlpy3
